0677e40443e676f6fdaaa3080398e50781660034
Gerhard Koekemoer <gerhardk@prodispace.co.za>
2011-11-15 11:38:55 +0200

    config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Battleships::Application.config.secret_token = '711ef22750d12b41842d48a41b61332d8aa94be438dac9d16634feaf0199711b24c17eaff84da5578668d05c42832e675312abe9565e1b308d56ad09247f6525'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Battleships::Application.config.session_store :cookie_store, key: '_battleships_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Battleships::Application.config.session_store :active_record_store
