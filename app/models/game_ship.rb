7f8c67442008d8e27d9615dc3151c8f29ac21c86
Gerhard Koekemoer <gerhardk@prodispace.co.za>
2011-11-23 16:47:33 +0200

    before create
    do not create more than max ships per game

--- app/models/game_ship.rb
+++ app/models/game_ship.rb
@@ -7,8 +7,9 @@ class GameShip < ActiveRecord::Base
   def check_for_max_ships
     ship = Ship.find(self.ship_id)
 
-    if GameShip.where(:ship_id => ship.id).count >= ship.max_per_game
-      Game.errors.add(:base, "Max Number of ships per game reached")
+    if GameShip.where(:ship_id => ship.id, :game_id => self.game_id).count >= ship.max_per_game
+      game = Game.find(self.game_id)
+      game.errors.add(:base, "Max Number of ships per game reached")
       return false
     end
   end
