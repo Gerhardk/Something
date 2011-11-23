49f6b3af0a2e583ac3692ff7d4f278d16cf56a86
Gerhard Koekemoer <gerhardk@prodispace.co.za>
2011-11-23 21:50:35 +0200

    javascript
    games create the correct ship for the game

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -7,17 +7,34 @@ function ShipsDraggables() {
 
 function ShipsDroppables() {
     $(".player_placement").droppable({
+
         activeClass: 'active',
         hoverClass: "hover",
         accept: ".ships",
         drop: function(event, ui) {
             var data_for = {}
-
+            var data_length = ui.draggable.attr("data_length")
             var x = $(this).attr("data_x");
             var y = $(this).attr("data_y");
+            var orientation = ui.draggable.attr("orientation");
+
+            if (data_length == 5 && orientation== "horizontal"){
+                x = x - 2
+
+            } else if((data_length == 4 || data_length == 3 || data_length == 2)  && orientation== "horizontal") {
+                x = x - 1
+
+            } else if(data_length == 5 && orientation== "vertical"){
+                y = y - 2
+
+            }else if((data_length == 4 || data_length == 3 || data_length == 2)  && orientation== "vertical"){
+                y = y - 2
+
+            }
+
             var position = $(this).position();
             var ship_id = ui.draggable.attr("ship_id");
-            var orientation = ui.draggable.attr("orientation");
+
             var game_id = ui.draggable.attr("game_id");
             data_for= {
                 game_ship: {
