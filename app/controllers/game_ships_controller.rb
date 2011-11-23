0043dab59d3668454271aa3f91f4d33e7fe14597
Gerhard Koekemoer <gerhardk@prodispace.co.za>
2011-11-23 16:47:45 +0200

    controller jumbo

--- app/controllers/game_ships_controller.rb
+++ app/controllers/game_ships_controller.rb
@@ -1,7 +1,13 @@
 class GameShipsController < InheritedResources::Base
-
+  respond_to :js, :html, :xml
   def create
+    @game = Game.find(params[:game_ship][:game_id])
+
     @game_ship = GameShip.create(params[:game_ship])
-    render :nothing => true
+    if @game_ship.id != nil
+      render :nothing => true
+    else
+      render :template => "game_ships/errors.js.erb"
+    end
   end
 end
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,15 @@
 class GamesController < InheritedResources::Base
 
   def register
-    @game = Game.create(params[:game])
+    @game = Game.find(params[:id])
     flash[:notice] = "Place your ships"
     @game_ships = GameShip.where(:game_id => @game.id)
     render :register
+  end
+
+  def create
+    @game = Game.create(params[:game])
+    redirect_to register_game_path(@game)
   end
 
   def nuke
