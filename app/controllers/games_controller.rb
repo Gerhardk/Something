e03926b0b52b01b0781b2839ed362235fb1f1e56
Gerhard Koekemoer <gerhardk@prodispace.co.za>
2011-11-23 09:57:32 +0200

    games look and feel

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,9 @@
 class GamesController < InheritedResources::Base
 
   def register
-
+    @game = Game.create(params[:game])
+    flash[:notice] = "Place your ships"
+    redirect_to game_path(@game)
   end
 
   def nuke
