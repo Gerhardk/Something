dde19b9aebe89308867ec180b1d180afb5a59cf8
Gerhard <gerhard@aazajnbw0953.(none)>
2011-12-02 15:14:38 +0200

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Ship.create(:name => "Carrier", :length => 5, :max_per_game => 1)
+Ship.create(:name => "Battleship", :length => 4, :max_per_game => 1)
+Ship.create(:name => "Destroyer", :length => 3, :max_per_game => 1)
+Ship.create(:name => "Submarines", :length => 2, :max_per_game => 2)
+Ship.create(:name => "Patrol Boat", :length => 1, :max_per_game => 2)
